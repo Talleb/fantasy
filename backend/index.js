3c9b82dfc3e91e0ba1fe83af383087e08b51a49c
Talleb <taulant_09_@hotmail.com>
2020-03-02 14:24:10 +0100

    Uppdaterat backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const sqlite = require('sqlite')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+let database
+
+sqlite.open('fotboll.sqlite').then(database_ => {
+    database = database_
+})
+app.get('/', (request, response) => {
+    database.all('SELECT * FROM players').then(players => {
+        response.send(players)
+        console.log([players])
+    })
+})
+app.listen(3000)
