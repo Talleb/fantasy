2a0c7e6643826fcdebcfb3ca7123369ce024154b
Markus Wiland <markus.wiland@outlook.com>
2020-03-04 13:30:22 +0100

    highscore på backend

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const sqlite = require('sqlite')
-const cors = require('cors')
-const app = express()
-const PORT = 3000
+const express = require('express');
+const sqlite = require('sqlite');
+const cors = require('cors');
 
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
 
-app.listen(PORT)
+let database;
 
-//Making the database global
-let database
 sqlite.open('fotboll.sqlite').then(database_ => {
-    database = database_
-})
-
-//Configuring the API 
+  database = database_;
+});
 app.get('/', (request, response) => {
-    database.all('SELECT * FROM players').then(players => {
-        response.json(players)
-    })
-})
+  database.all('SELECT * FROM players').then(players => {
+    response.send(players);
+    console.log([players]);
+  });
+});
+app.get('/Highscore', (request, response) => {
+  database.all('SELECT * FROM Highscore').then(score => {
+    response.send(score);
+    console.log([players]);
+  });
+});
+app.listen(3000);
