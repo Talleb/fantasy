4444856b063a6596679fb19b1d95b0bc8c5d50eb
Markus Wiland <markus.wiland@outlook.com>
2020-03-04 12:27:03 +0100

    Homepage, created highscore table and button to draft

--- frontend/src/components/Layout.vue
+++ frontend/src/components/Layout.vue
@@ -1,16 +1,13 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
+    <v-navigation-drawer v-model="drawer" app>
       <v-list dense>
         <v-list-item link>
-          <v-list-item-action >
+          <v-list-item-action>
             <v-icon>mdi-home</v-icon>
           </v-list-item-action>
-          <v-list-item-content> 
-            <v-list-item-title >Ultimate Draft</v-list-item-title>
+          <v-list-item-content>
+            <v-list-item-title>Ultimate Draft</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
@@ -32,36 +29,31 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      color="green"
-      dark
-    >
+    <v-app-bar app color="green" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title @click="$router.push('/')">The Ultimate Draft</v-toolbar-title>
+      <v-toolbar-title @click="$router.push('/')"
+        >The Ultimate Draft</v-toolbar-title
+      >
     </v-app-bar>
 
     <v-content>
-        <router-view></router-view>
+      <router-view></router-view>
     </v-content>
 
-    <v-footer
-      color="green"
-      app
-    >
+    <v-footer color="green" app>
       <span class="white--text">&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    name: "Layout",
-    props: {
-      source: String,
-    },
-    data: () => ({
-      drawer: true,
-    }),
-  }
+export default {
+  name: "Layout",
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: true
+  })
+};
 </script>
