686d22287d005f04563bfa27fb49fa80f2be6753
Markus Wiland <markus.wiland@outlook.com>
2020-03-04 13:05:55 +0100

    push backend and frontend

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
-const express = require('express')
-const sqlite = require('sqlite')
-const cors = require('cors')
+const express = require('express');
+const sqlite = require('sqlite');
+const cors = require('cors');
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
-let database
+let database;
 
 sqlite.open('fotboll.sqlite').then(database_ => {
-    database = database_
-})
+  database = database_;
+});
 app.get('/', (request, response) => {
-    database.all('SELECT * FROM players').then(players => {
-        response.send(players)
-        console.log([players])
-    })
-})
-app.listen(3000)
+  database.all('SELECT * FROM players').then(players => {
+    response.send(players);
+    console.log([players]);
+  });
+});
+app.get('/Highscore', (request, response) => {
+  database.all('SELECT * FROM Highscore').then(score => {
+    response.send(score);
+    console.log([players]);
+  });
+});
+app.listen(3000);
